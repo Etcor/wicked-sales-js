5cd3fc8dd17cc47138c05b96aedd28aa06f99c1c
Ethan Cordes <ethancordes7@gmail.com>
2020-01-13 13:31:26 -0800

    add components to display products in db add product-list-item component that renders a card and populates card with information from props add product-list component that requests the product list from server and stores it within its state product-list component renders a product-list-item for each product returned from db add ProductList to render method of app.jsx

---
+++ client/components/product-list-item.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function ProductListItem(props) {
+  return (
+    <div
+      className="card"
+      style={{ height: '100%' }}>
+      <img
+        alt=""
+        className="card-img-top"
+        src={props.image}
+        style={{ objectFit: 'contain', height: '300px' }}
+      />
+      <div className="card-body">
+        <h5 className="card-title">
+          {props.name}
+        </h5>
+        <h6 className="card-subtitle text-muted pb-2">
+            ${props.price / 100}
+        </h6>
+        <p className="card-text">
+          {props.shortDescription}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default ProductListItem;
---
+++ client/components/product-list.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import ProductListItem from './product-list-item';
+
+class ProductList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('/api/products')
+      .then(response => response.json())
+      .then(products => {
+        this.setState({ products });
+      })
+      .catch(err => console.error(err));
+  }
+
+  render() {
+    const { products } = this.state;
+    return (
+      <div className="contain-fluid bg-light">
+        <div className="container pt-5">
+          <div className="row">
+            {products.map(product => {
+              return (
+                <div
+                  key={product.productId}
+                  className="col-lg-4 col-md-6 col-sm-12 mb-4">
+                  <ProductListItem
+                    name={product.name}
+                    image={product.image}
+                    price={product.price}
+                    shortDescription={product.shortDescription}
+                  />
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ProductList;
