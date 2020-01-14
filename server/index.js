aca16e6adae40d0233406d077fa99a9baea90a59
Ethan Cordes <ethancordes7@gmail.com>
2020-01-14 13:34:55 -0800

    add get and post endpoints for /api/cart add get endpoint that looks for an existing cartId in the req.session object add post endpoint that checks if the productId is a valid Id and that it exists if the productId exists in the db and a cart does not exist for the user, a new cart will be created when the cart is created, it will save the unique cartId into the req.session object it will then return the product that was added as well as the cartItemId for the product that was added to cart

--- server/index.js
+++ server/index.js
@@ -51,6 +51,100 @@ app.get('/api/products/:productId', (req, res, next) => {
     .catch(err => next(err));
 });
 
+app.get('/api/cart', (req, res, next) => {
+  const { cartId } = req.session;
+  if (!cartId) {
+    res.status(200).send([]);
+  }
+  const sql = `
+        select "c"."cartItemId",
+          "c"."price",
+          "p"."productId",
+          "p"."image",
+          "p"."name",
+          "p"."shortDescription"
+      from "cartItems" as "c"
+      join "products" as "p" using ("productId")
+    where "c"."cartId" = $1
+  `;
+  const params = [cartId];
+  db.query(sql, params)
+    .then(response => res.status(200).json(response.rows))
+    .catch(err => next(err));
+});
+
+app.post('/api/cart', (req, res, next) => {
+  const { productId } = req.body;
+  if (productId > 0 && typeof parseInt(productId) === 'number') {
+    const params = [productId];
+    const sql = `
+      select "price"
+      from "products"
+      where "productId" = $1`;
+
+    db.query(sql, params)
+      .then(result => {
+        const products = result.rows;
+
+        if (products.length === 0) {
+          return next(new ClientError('There are no products by that Id.', 400));
+        } else if (req.session.cartId) {
+          const { price } = products[0];
+          const { cartId } = req.session;
+          return { cartId, price };
+        }
+        const sql = `
+          insert into "carts" ("cartId", "createdAt")
+          values (default, default)
+          returning "cartId"
+        `;
+        return (
+          db.query(sql)
+            .then(result => {
+              const { cartId } = result.rows[0];
+              const { price } = products[0];
+              return { cartId, price };
+            })
+        );
+      })
+      .then(cart => {
+        const { price, cartId } = cart;
+        req.session.cartId = cartId;
+
+        const sql = `
+          insert into "cartItems" ("cartId", "productId", "price")
+          values ($1, $2, $3)
+          returning "cartItemId"
+        `;
+        const params = [cartId, productId, price];
+
+        return db.query(sql, params);
+      })
+      .then(result => {
+        const { cartItemId } = result.rows[0];
+        const sql = `
+          select "c"."cartItemId",
+                 "c"."price",
+                 "p"."productId",
+                 "p"."image",
+                 "p"."name",
+                 "p"."shortDescription"
+            from "cartItems" as "c"
+            join "products" as "p" using ("productId")
+           where "c"."cartItemId" = $1
+        `;
+        const params = [cartItemId];
+        db.query(sql, params)
+          .then(result => {
+            res.status(201).json(result.rows[0]);
+          });
+      })
+      .catch(err => next(err));
+  } else {
+    return next(new ClientError('Product ID must be a positive integer.', 400));
+  }
+});
+
 app.use('/api', (req, res, next) => {
   next(new ClientError(`cannot ${req.method} ${req.originalUrl}`, 404));
 });
